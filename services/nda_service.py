9ea86e3c3ac38e6b8cf624de93c62ec90c800fbb
lawchatai <lawchat.indie@gmail.com>
2025-12-28 23:29:38 +0530

    doc

--- services/nda_service.py
+++ services/nda_service.py
@@ -1,55 +1,3 @@
-# def generate_employment_nda(data):
-#     employer_name = data.get("employer_name", [""])[0]
-#     employer_type = data.get("employer_type", [""])[0]
-#     employee_name = data.get("employee_name", [""])[0]
-#     designation = data.get("designation", [""])[0]
-#     effective_date = data.get("effective_date", [""])[0]
-#     jurisdiction = data.get("jurisdiction", [""])[0]
-#     post_duration = data.get("post_duration", [""])[0]
-#
-#     confidential_items = data.get("confidential[]", [])
-#
-#     confidential_text = ", ".join(confidential_items)
-#
-#     nda = f"""
-# EMPLOYEE CONFIDENTIALITY AND NON-DISCLOSURE AGREEMENT
-#
-# This Agreement is made on {effective_date}
-#
-# BETWEEN
-#
-# {employer_name}, a {employer_type},
-# (hereinafter referred to as the "Employer")
-#
-# AND
-#
-# {employee_name}, employed as {designation},
-# (hereinafter referred to as the "Employee")
-#
-# 1. CONFIDENTIAL INFORMATION
-# The Employee agrees that all confidential information including
-# {confidential_text} shall remain strictly confidential.
-#
-# 2. PURPOSE
-# Confidential information shall be used solely for employment purposes.
-#
-# 3. TERM
-# This Agreement shall continue during employment and for {post_duration} years
-# after termination.
-#
-# 4. RETURN OF PROPERTY
-# Upon termination, the Employee shall return all confidential material.
-#
-# 5. GOVERNING LAW
-# This Agreement shall be governed by the laws of India.
-# Courts at {jurisdiction} shall have exclusive jurisdiction.
-#
-# IN WITNESS WHEREOF,
-# The parties have executed this Agreement on the date first written above.
-# """
-#
-#     return nda.strip()
-
 def generate_employment_nda(data):
     employer_name = data.get("employer_name", [""])[0]
     employee_name = data.get("employee_name", [""])[0]
@@ -61,8 +9,6 @@ def generate_employment_nda(data):
     confidential_text = ", ".join(confidential_items)
 
     nda = f"""
-EMPLOYEE NONDISCLOSURE AGREEMENT
-
 This agreement (the “Agreement”) is entered into on {effective_date} by {employer_name} (“Company”) 
 and {employee_name}, employed as {designation} (“Employee”).
 
@@ -112,10 +58,16 @@ Employee is provided notice that they shall not be held criminally or civilly li
 
 8. Signatures
 
-Employee                                                                     Company
+Employee                                                  
 
-Signature _____________________                                              Signature _____________________
-Name __________________________                                              Name __________________________
-Date __________________________                                              Date __________________________
+Signatures _____________________
+Print Name _____________________
+Date _____________________
+
+Company
+
+Signatures _____________________
+Print Name _____________________
+Date _____________________
 """
     return nda.strip()
